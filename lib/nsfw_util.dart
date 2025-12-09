8f5e63b4957a16244db0bc2f7af0e76c251128e1
sanin <saninthottungalhere@gmail.com>
2025-12-09 21:29:54 +0530

    initialize method added

--- lib/nsfw_util.dart
+++ lib/nsfw_util.dart
@@ -1,3 +1,27 @@
+import 'dart:io';
+
+import 'package:nsfw_util/src/utils/asset_utils.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
+
 class NSFWUtil {
-  Future<void> initialize() async {}
+  late final Interpreter _interpreter;
+  late final Tensor _inputTensor;
+  late final Tensor _outputTensor;
+
+  Future<void> initialize() async {
+    final options = InterpreterOptions();
+
+    if (Platform.isAndroid) {
+      options.addDelegate(XNNPackDelegate());
+    }
+
+    if (Platform.isIOS) {
+      options.addDelegate(GpuDelegate());
+    }
+
+    _interpreter = await Interpreter.fromAsset(Assets.model, options: options);
+
+    _inputTensor = _interpreter.getInputTensors().first;
+    _outputTensor = _interpreter.getOutputTensors().first;
+  }
 }
---
+++ lib/src/utils/asset_utils.dart
@@ -0,0 +1,6 @@
+class Assets {
+  const Assets._();
+
+  static const String model = 'assets/models/nsfw.tflite';
+  static const String labels = 'assets/models/labels.txt';
+}
